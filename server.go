06046e8b6197a846e263c092c28cdd189679d611
William Kennedy <bill@ardanstudios.com>
2015-02-10 18:03:10 -0500

    Update server.go
    In progress

--- server.go
+++ server.go
@@ -6,7 +6,8 @@ import (
 	"net/rpc"
 )
 
-// TODO move common code to shared lib
+// Same as before, only if these work together maybe group them.
+
 type RegisterArgs struct {
 	Worker string
 }
@@ -23,24 +24,20 @@ type DoTaskReply struct {
 	OK bool
 }
 
-func Dial(host string, rpcname string,
-	args interface{}, reply interface{}) bool {
+func Dial(host string, rpcname string, args interface{}, reply interface{}) bool {
 	c, err := rpc.Dial("tcp", host)
 	if err != nil {
 		return false
 	}
 	defer c.Close()
 
-	err = c.Call(rpcname, args, reply)
-	if err == nil {
-		return true
+	if err := c.Call(rpcname, args, reply) err != nil {
+		log.Println(err)
+		return false
 	}
 
-	log.Println(err)
-	return false
+	return true
 }
-
-/// end common things that need to move to lib
 
 type Master struct {
 	l               net.Listener
@@ -51,7 +48,7 @@ type Master struct {
 
 // Register workers
 func (m *Master) Register(args *RegisterArgs, rep *RegisterReply) error {
-	log.Printf("Registering worker node: %s\n", args.Worker)
+	log.Println("Registering worker node:", args.Worker)
 	go func() { // TODO: do i need a goroutine here, clients don't seem to get a response w/o it.
 		m.registerChannel <- args.Worker
 	}()
@@ -60,50 +57,53 @@ func (m *Master) Register(args *RegisterArgs, rep *RegisterReply) error {
 }
 
 func InitMaster() *Master {
-	m := new(Master)
-	m.isAlive = true
-	m.doneChan = make(chan bool)
-	m.registerChannel = make(chan string)
-	return m
+	return &Master{
+		isAlive: true,
+		doneChan: make(chan bool),
+		registerChannel: make(chan string),
+	}
 }
 
 func (m *Master) initRPCServer() {
 	rpc.Register(m)
 
-	listener, e := net.Listen("tcp", ":1234")
+	var err error
+	m.l, err = net.Listen("tcp", ":1234")
 	if e != nil {
 		log.Fatal("listen error:", e)
 	}
-	m.l = listener
 
 	// accept connections on seperate thread.
 	go func() {
 		for m.isAlive {
 			conn, err := m.l.Accept()
-			if err == nil {
-				go func() {
-					log.Println("serving request")
-					rpc.ServeConn(conn)
-					conn.Close()
-				}()
-			} else {
+			if err != nil {
 				log.Println("errors in go routine")
+				continue
 			}
+			
+			go func() {
+				log.Println("serving request")
+				rpc.ServeConn(conn)
+				conn.Close()
+			}()
 		}
 	}()
 }
 
-// Ranges over lists of workers and asks each of them to DoWork
 func (m *Master) doWork() {
 	log.Println("Starting to farm out work to workers...")
 
 	for w := range m.registerChannel {
-		log.Printf("Got a worker %s", w)
-		// Synchronous call
-		args := &DoTaskArgs{"task A"}
+		log.Println("Got a worker", w)
+		
 		var reply DoTaskReply
-		Dial("127.0.0.1:1235", "Worker.DoTask", args, &reply)
-		log.Printf("Result from worker: %v", reply.OK)
+		args := DoTaskArgs{
+			name: "task A"
+		}
+		
+		Dial("127.0.0.1:1235", "Worker.DoTask", &args, &reply)
+		log.Println("Result from worker:", reply.OK)
 	}
 
 	// program will exit when DoneChan = true
