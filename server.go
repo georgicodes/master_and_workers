56610211707ba34494768d4202465ebc5ef45b0f
William Kennedy <bill@ardanstudios.com>
2015-02-10 19:05:09 -0500

    Update server.go
    This has been run through go build, go vet and golint. I tested it with your client. Continue to use <ctrl> C to shutdown the server cleanly. Make sure there are no race conditions as you go as well.

--- server.go
+++ server.go
@@ -1,117 +1,154 @@
+// Package main provides a sample server program.
 package main
 
 import (
 	"log"
 	"net"
 	"net/rpc"
+	"os"
+	"os/signal"
+	"sync"
 )
 
-// TODO move common code to shared lib
+// RegisterArgs ...
 type RegisterArgs struct {
 	Worker string
 }
 
+// RegisterReply ...
 type RegisterReply struct {
 	OK bool
 }
 
+// DoTaskArgs ...
 type DoTaskArgs struct {
 	Name string
 }
 
+// DoTaskReply ...
 type DoTaskReply struct {
 	OK bool
 }
 
-func Dial(host string, rpcname string,
-	args interface{}, reply interface{}) bool {
+// Dial connects into this server to run some tests.
+func Dial(host string, rpcname string, args interface{}, reply interface{}) bool {
 	c, err := rpc.Dial("tcp", host)
 	if err != nil {
 		return false
 	}
 	defer c.Close()
 
-	err = c.Call(rpcname, args, reply)
-	if err == nil {
-		return true
+	if err := c.Call(rpcname, args, reply); err != nil {
+		log.Println(err)
+		return false
 	}
 
-	log.Println(err)
-	return false
+	return true
 }
 
-/// end common things that need to move to lib
-
+// Master ...
 type Master struct {
-	l               net.Listener
-	isAlive         bool
-	doneChan        chan bool
-	registerChannel chan string
+	l        net.Listener
+	wg       sync.WaitGroup
+	register chan string
 }
 
-// Register workers
+// NewMaster creates a new Master value.
+func NewMaster() *Master {
+	return &Master{
+		register: make(chan string),
+	}
+}
+
+// Register adds a worker to the list of clients that exist.
 func (m *Master) Register(args *RegisterArgs, rep *RegisterReply) error {
-	log.Printf("Registering worker node: %s\n", args.Worker)
-	go func() { // TODO: do i need a goroutine here, clients don't seem to get a response w/o it.
-		m.registerChannel <- args.Worker
-	}()
+	log.Println("Registering worker node:", args.Worker)
+
+	// Tell the Master this worker exists.
+	m.register <- args.Worker
+
 	rep.OK = true
 	return nil
 }
 
-func InitMaster() *Master {
-	m := new(Master)
-	m.isAlive = true
-	m.doneChan = make(chan bool)
-	m.registerChannel = make(chan string)
-	return m
-}
-
+// initRPCServer
 func (m *Master) initRPCServer() {
 	rpc.Register(m)
 
-	listener, e := net.Listen("tcp", ":1234")
-	if e != nil {
-		log.Fatal("listen error:", e)
+	var err error
+	if m.l, err = net.Listen("tcp", ":1234"); err != nil {
+		log.Fatal("listen error:", err)
 	}
-	m.l = listener
 
-	// accept connections on seperate thread.
+	m.wg.Add(1)
+
+	// Accept connections on seperate goroutine.
 	go func() {
-		for m.isAlive {
+		for {
 			conn, err := m.l.Accept()
-			if err == nil {
-				go func() {
-					log.Println("serving request")
-					rpc.ServeConn(conn)
-					conn.Close()
-				}()
-			} else {
-				log.Println("errors in go routine")
+			if err != nil {
+				log.Println("Shutting down Accept handler.")
+				// Assume this has an error because we are shutting down.
+				m.wg.Done()
+				return
 			}
+
+			go func() {
+				log.Println("Client has connected.")
+				rpc.ServeConn(conn)
+				log.Println("Client has disconnected.")
+				conn.Close()
+			}()
 		}
 	}()
 }
 
-// Ranges over lists of workers and asks each of them to DoWork
+// doWork for now takes a registration and uses it as a task.
 func (m *Master) doWork() {
 	log.Println("Starting to farm out work to workers...")
 
-	for w := range m.registerChannel {
-		log.Printf("Got a worker %s", w)
-		// Synchronous call
-		args := &DoTaskArgs{"task A"}
-		var reply DoTaskReply
-		Dial("127.0.0.1:1235", "Worker.DoTask", args, &reply)
-		log.Printf("Result from worker: %v", reply.OK)
-	}
+	m.wg.Add(1)
+	go func() {
+		for w := range m.register {
+			log.Println("Got a worker", w)
 
-	// program will exit when DoneChan = true
-	<-m.doneChan
+			var reply DoTaskReply
+			args := DoTaskArgs{
+				Name: "task A",
+			}
+
+			Dial("127.0.0.1:1235", "Worker.DoTask", &args, &reply)
+			log.Println("Result from worker:", reply.OK)
+		}
+
+		log.Println("Shutting down doWork handler.")
+		m.wg.Done()
+	}()
+}
+
+// close shutdowns all the goroutines. This does not take an accouting
+// of existing client connections at this time.
+func (m *Master) close() {
+	// Kill the listener.
+	m.l.Close()
+
+	// Kill doWork.
+	close(m.register)
+
+	// Wait for those to call Done.
+	m.wg.Wait()
 }
 
 func main() {
-	m := InitMaster()
+	m := NewMaster()
 	m.initRPCServer()
 	m.doWork()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	<-sigChan
+
+	log.Println("Shutting Down Started")
+	m.close()
+	log.Println("Shutting Down Completed")
 }
